56a246f8ac45ed4540e983479918e03de7ec5e14
Gabriel Llamas <gabriel_llamas_llopis@yahoo.es>
2014-01-19 14:47:03 +0100

    docs

--- examples/user-extensions.js
+++ examples/user-extensions.js
@@ -1,7 +1,7 @@
 "use strict";
 
 /*
-This example demonstrate how to send and receive custom user extensions to
+This example demonstrates how to send and receive custom user extensions to
 extend the protocol in some way and adapting it to your needs.
 
 The client sends a number and the server substracts one and returns it back to
