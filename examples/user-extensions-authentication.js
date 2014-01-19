bfde077abd0712f95573407588817a2caca6269f
Gabriel Llamas <gabriel_llamas_llopis@yahoo.es>
2014-01-19 11:43:25 +0100

    added examples

--- examples/user-extensions-authentication.js
+++ examples/user-extensions-authentication.js
@@ -3,8 +3,44 @@
 /*
 Basic authentication over TFTP using the user extensions. The user extensions
 are transmitted in plain text so an encrypt algorithm would be nice to encrypt
-the password (a symmetric cipher would suffice for simple tasks).
+the password (a symmetric key-based cipher would suffice for simple tasks).
 */
 
+var fs = require ("fs");
 var tftp = require ("../lib");
 
+var users = {
+  usr1: "usr1-pass"
+};
+
+var server = tftp.createServer (function (req, res){
+  req.on ("stats", function (stats){
+    if (!stats.userExtensions || !stats.userExtensions.user ||
+        !stats.userExtensions.pass ||
+        users[stats.userExtensions.user] !== stats.userExtensions.pass){
+      return req.abort ("Invalid user");
+    }
+  });
+  this.requestListener (req, res);
+})
+server.listen ();
+
+fs.openSync ("tmp1", "w");
+
+var client = tftp.createClient ();
+client.get ("tmp1", "tmp2", function (error){
+  //Invalid user
+  console.error (error);
+  
+  client.get ("tmp1", "tmp2", { userExtensions: {
+    user: "usr1",
+    pass: "usr1-pass"
+  }}, function (error){
+    if (error) return console.error (error);
+    
+    console.log ("OK");
+    server.close ();
+    fs.unlinkSync ("tmp1");
+    fs.unlinkSync ("tmp2");
+  });
+});
--- examples/user-extensions.js
+++ examples/user-extensions.js
@@ -8,5 +8,28 @@ The client sends a number and the server substracts one and returns it back to
 the client.
 */
 
+var fs = require ("fs");
 var tftp = require ("../lib");
 
+var server = tftp.createServer (function (req, res){
+  req.on ("stats", function (stats){
+    res.setUserExtensions ({ num: parseFloat (stats.userExtensions.num) - 1 });
+  });
+  this.requestListener (req, res);
+})
+server.listen ();
+
+fs.openSync ("tmp1", "w");
+console.log (">> 3");
+
+tftp.createClient ()
+    .createGetStream ("tmp1", { userExtensions: { num: 3 } })
+    .on ("stats", function (stats){
+      console.log ("<< " + stats.userExtensions.num);
+    })
+    .pipe (fs.createWriteStream ("tmp2"))
+    .on ("finish", function (){
+      server.close ();
+      fs.unlinkSync ("tmp1");
+      fs.unlinkSync ("tmp2");
+    });
